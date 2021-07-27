a940136328fc024678b4862d6b27e236844d78a5
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2021-07-27 18:05:55 -0400

    working on js

--- js/main.js
+++ js/main.js
@@ -625,15 +625,41 @@
       event.preventDefault();
       // Waiting for the response from the server
       submitBtn.html('Wait...').addClass('wait').prop('disabled', true);
-      submitBtn.removeClass('wait').html('Success').addClass('success');
-      feedbackEl.addClass('success').html('Thank you for your message. It has been sent.').fadeIn(1200);
       setTimeout(function() {
-        submitBtn.html(submitBtnText).removeClass('success').prop('disabled', false);
-        feedbackEl.fadeOut(1200).removeClass('success').html('');
-      }, 6000);
-      // Clears the Form
-      form[0].reset();
+        // Posts the Form's data to the server using Ajax
+        $.ajax({
+            url: form.attr('action'),
+            type: 'POST',
+            data: form.serialize(),
+          })
+          // Getting a response from the server
+          .done(function(response) {
+            // If the PHP file succeed sending the message
+            if (response == 'success') {
+              // Feedback to the user
+              submitBtn.removeClass('wait').html('Success').addClass('success');
+              feedbackEl.addClass('success').html('Thank you for your message. It has been sent.').fadeIn(200);
+              setTimeout(function() {
+                submitBtn.html(submitBtnText).removeClass('success').prop('disabled', false);
+                feedbackEl.fadeOut(200).removeClass('success').html('');
+              }, 6000);
+              // Clears the Form
+              form[0].reset();
+              // If something is wrong
+            } else {
+              // Feedback to the user
+              console.log(response);
+              submitBtn.removeClass('wait').html('Error').addClass('error');
+              feedbackEl.addClass('error').html('Server error! Please check your browser console log for more details.').fadeIn(200);
+              setTimeout(function() {
+                submitBtn.html(submitBtnText).removeClass('error').prop('disabled', false);
+                feedbackEl.fadeOut(200).removeClass('error').html('');
+              }, 6000);
+            }
+          });
+      }, 1000);
     });
+    
 
   });
   $(window).on('load', function() {
